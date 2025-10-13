1478d934509c96edcbf94ab36a3bab55a1aa2bf9
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-10-13 18:04:44 +0500

    add fix

--- post/prisma/migrations/20251009101717_init2/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Book" ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT true;
--- post/src/api/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaModule } from 'src/prisma/prisma.module';
-import { BookModule } from './book/book.module';
-
-@Module({
-  imports: [PrismaModule, BookModule],
-})
-export class AppModule { }
